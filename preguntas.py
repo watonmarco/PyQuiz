e766195769de6af92f38aa681de62bd269f4a78b
marco <129217375+watonmarco@users.noreply.github.com>
2024-05-31 11:09:59 -0400

    mayúsculas en la primera pregunta

--- preguntas.py
+++ preguntas.py
@@ -1,6 +1,6 @@
 DATA = [
     {
-        "Pregunta":"¿cuanto es 2 + 2?",
+        "Pregunta":"¿Cuánto es 2 + 2?",
         "Opciones":["5", "1", "2", "4"],
         "Respuesta":3},  # El índice de la respuesta correcta en la lista de opciones
     {
